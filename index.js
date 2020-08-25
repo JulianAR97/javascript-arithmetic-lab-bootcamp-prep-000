94b38c7d902d03aaeac17a73f98c0092c93f36d2
whimsical-logger-3153 <julianrosenthal@yahoo.com>
2020-08-25 22:38:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,21 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a / b; 
+  return a / b;
+}
+
+function increment(n) {
+  return n++;
+}
+
+function decrement(n) {
+  return n--;
+}
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
 }
